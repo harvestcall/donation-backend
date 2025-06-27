a85d59655f30d7267ce7b6dc13160f3d7c1b2ed0
Harvest Call Ministries <tech@harvestcallafrica.org>
2025-06-27 04:53:06 +0100

    Switch to PostgreSQL on Render

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,13 @@ module.exports = {
       filename: './donation.db'
     },
     useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
